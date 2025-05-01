d75024a3bf6fe852c92a1bd5ac6920c49bd330fd
buhtig-sudo-azar <azarovvvit@mail.ru>
2025-05-01 04:59:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from './components/Greeting.jsx';
+
+
+  
+function App() {
+    return (
+        <div>
+            <Greeting />
+        </div>
+    );
+}
+
+export default App;
