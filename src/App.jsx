4b8e0bc1f5054b91233affaeafd5a0dc33c7fd64
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-05-26 02:59:04 +0000

    Keys In React

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Greeting from './components/Greeting.jsx';
+import ListItems from './components/ListItems.jsx';
 
 
   
 function App() {
     return (
         <div>
-            <Greeting />
+            <ListItems />
         </div>
     );
 }
